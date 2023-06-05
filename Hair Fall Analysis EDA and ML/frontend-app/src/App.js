cebda6ee9e7fc622ea8b30dbce2307211832dc63
mithilraj13 <82027839+mithilraj13@users.noreply.github.com>
2023-06-05 23:20:52 +0530

    Add files via upload

---
+++ Hair Fall Analysis EDA and ML/frontend-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import DataCollection from "./Components/DataCollection";
+
+function App() {
+  return (
+    <div className="App">
+      <DataCollection />
+    </div>
+  );
+}
+
+export default App;
